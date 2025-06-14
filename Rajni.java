6010efe05430c24f664b8b5b745e82d52dd06d24
venkat <prasadakula4488@gmail.com>
2025-06-14 14:55:26 +0530

    Mahesh babu came

--- Rajni.java
+++ Rajni.java
@@ -3,5 +3,6 @@ public class Rajni{
     public static void main(String[] args) {
         System.out.println("Rajni da");
         System.out.println("Thee Thalapathy!!!");
+        System.out.println("Jai babu");
     }
 }
