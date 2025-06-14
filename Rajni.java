0d0131d7d8e4e2a525535451d1510d22b94b4a09
venkat <prasadakula4488@gmail.com>
2025-06-14 14:50:18 +0530

    thalapathy added

--- Rajni.java
+++ Rajni.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class Rajni{
     public static void main(String[] args) {
         System.out.println("Rajni da");
+        System.out.println("Thee Thalapathy!!!");
     }
 }
